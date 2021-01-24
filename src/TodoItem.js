b1d72fe958270ee526af74e5dd223d8d1c50c5cb
William Chen <seed45699@gmail.com>
2021-01-24 22:50:42 +0800

    Refactor TodoItem with styled-component

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,6 +5,80 @@
 
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TodoItemWrapper = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  padding: 0.75rem 1.25rem;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+`;
+
+const TodoItemCheckbox = styled.input.attrs(({ defaultChecked, id }) => ({
+  type: 'checkbox',
+  defaultChecked,
+  id: id || '',
+}))`
+  margin-right: 0.5rem;
+`;
+
+const TodoItemEditInput = styled.input`
+  margin-left: 0.5rem;
+`;
+
+const TodoItemLabel = styled.label.attrs(({ htmlFor }) => ({
+  htmlFor,
+}))`
+  ${({ isDone }) => isDone && `text-decoration: line-through;`}
+`;
+
+const TodoItemContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  font-weight: 400;
+  color: #212529;
+  text-align: center;
+  vertical-align: middle;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s;
+  outline: none;
+`;
+
+const WarningButton = styled(Button)`
+  background-color: #ffc107;
+  border: 1px solid #ffc107;
+  color: black;
+  &:focus {
+    border-color: #ffc107;
+  }
+`;
+
+const DangerButton = styled(Button)`
+  background-color: #dc3545;
+  border: 1px solid #dc3545;
+  color: white;
+  &:focus {
+    border-color: #dc3545;
+  }
+`;
+
+const TodoItemButtons = styled.div`
+  ${Button} ~ ${Button} {
+    margin-left: 1rem;
+  }
+`;
 
 export default function TodoItem({
   todo,
@@ -12,7 +86,6 @@ export default function TodoItem({
   onDeleteTodoClick,
   onToggleDoneTodoClick,
 }) {
-  // 我記得用 prop 當作 component 中 state 並不是好的設計方式，但又不知道除此之外能怎麼做比較好
   const [todoEditInputValue, setTodoEditInputValue] = useState(todo.todo);
 
   const onEditTodoChange = (e) => {
@@ -20,48 +93,34 @@ export default function TodoItem({
   };
 
   return (
-    <li className="list-group-item" data-id={todo.id}>
-      <div className="d-flex justify-content-between align-items-baseline">
-        <div className="d-flex align-items-baseline">
-          <div onClick={() => onToggleDoneTodoClick(todo.id)}>
-            <input
-              type="checkbox"
-              className="mr-2 todo-mark"
-              id={todo.id}
-              defaultChecked={todo.isDone}
-            />
-            <label className={todo.isDone ? 'done' : ''} htmlFor={todo.id}>
-              {todo.todo}
-            </label>
-          </div>
-          {todo.isEdit && (
-            <input
-              type="text"
-              className="ml-2 form-group"
-              name="edit"
-              value={todoEditInputValue}
-              onChange={onEditTodoChange}
-            />
-          )}
-        </div>
-        <div>
-          <button
-            className="btn btn-warning mr-3"
-            type="button"
-            onClick={() => onEditTodoClick(todo.id, todoEditInputValue)}
-          >
-            {todo.isEdit ? '完成編輯' : '編輯'}
-          </button>
-          <button
-            className="btn btn-danger mr-3"
-            type="button"
-            onClick={() => onDeleteTodoClick(todo.id)}
-          >
-            刪除
-          </button>
-        </div>
+    <TodoItemWrapper>
+      <div className="d-flex align-items-baseline">
+        <TodoItemContent onClick={() => onToggleDoneTodoClick(todo.id)}>
+          {/* 練習用 styled-component attr */}
+          <TodoItemCheckbox defaultChecked={todo.isDone} id={todo.id} />
+          <TodoItemLabel htmlFor={todo.id}>{todo.todo}</TodoItemLabel>
+        </TodoItemContent>
+        {todo.isEdit && (
+          // 不透過 attr 直接傳 input 也行
+          <TodoItemEditInput
+            type="text"
+            value={todoEditInputValue}
+            onChange={onEditTodoChange}
+          />
+        )}
       </div>
-    </li>
+
+      <TodoItemButtons>
+        <WarningButton
+          onClick={() => onEditTodoClick(todo.id, todoEditInputValue)}
+        >
+          {todo.isEdit ? '完成編輯' : '編輯'}
+        </WarningButton>
+        <DangerButton onClick={() => onDeleteTodoClick(todo.id)}>
+          刪除
+        </DangerButton>
+      </TodoItemButtons>
+    </TodoItemWrapper>
   );
 }
 
