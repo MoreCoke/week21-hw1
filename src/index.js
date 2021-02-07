a63cf542caea3c0bd41c14b8d03071c0c39cca0a
William Chen <seed45699@gmail.com>
2021-02-07 22:56:58 +0800

    Feat RestStyle

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { ResetStyle } from './style';
 import TodoList from './TodoList';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetStyle />
     <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
