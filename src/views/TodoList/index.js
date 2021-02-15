0b9594b36e981964eb040899e7b1d941336f934b
William Chen <seed45699@gmail.com>
2021-02-15 13:40:36 +0800

    Refactor hooks

--- src/views/TodoList/index.js
+++ src/views/TodoList/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable arrow-parens */
 /* eslint-disable react/jsx-one-expression-per-line */
 
-import React, { useState, useEffect, useMemo } from 'react';
+import React from 'react';
 
 import { Container, FilterButtons, List } from 'views/TodoList/style';
 import TodoItem from 'views/TodoList/components/TodoItem';
@@ -12,87 +12,30 @@ import {
   InfoButton,
   DangerButton,
 } from 'components/Button';
+import useTodo from 'hooks/useTodo';
 
 function TodoList() {
-  const [todoList, setTodoList] = useState([]);
-  const [todoType, setTodoType] = useState('all');
-
-  useEffect(() => {
-    console.log(
-      'todoList',
-      todoList.map((e) => e.isDone)
-    );
-  }, [todoList]);
-
-  const onAddTodoClick = (title) => {
-    const todo = {
-      id: new Date().getTime(),
-      todo: title,
-      isDone: false,
-      isEdit: false,
-    };
-    setTodoList((prevTodoList) => [todo, ...prevTodoList]);
-  };
-
-  const onEditTodoClick = (id, editValue) => {
-    const newTodoList = todoList.map((element) => {
-      if (element.id === id) {
-        return { ...element, isEdit: !element.isEdit, todo: editValue };
-      }
-      return element;
-    });
-    setTodoList(newTodoList);
-  };
-
-  const onDeleteTodoClick = (id) => {
-    const newTodoList = todoList.filter((element) => element.id !== id);
-    setTodoList(newTodoList);
-  };
-
-  const onToggleDoneTodoClick = (id) => {
-    const newTodoList = todoList.map((element) => {
-      if (element.id === id) {
-        return { ...element, isDone: !element.isDone };
-      }
-      return element;
-    });
-    setTodoList(newTodoList);
-  };
-
-  const filterTodoList = useMemo(() => {
-    switch (todoType) {
-      case 'all':
-        return todoList;
-      case 'done':
-        return todoList.filter((element) => element.isDone);
-      case 'undone':
-        return todoList.filter((element) => !element.isDone);
-      // case 'deleteAll':
-      //   return [];
-      default:
-        throw new Error("can't find todo filter type!");
-    }
-  }, [todoList, todoType]);
-
+  const {
+    filterTodoList,
+    onAddTodoClick,
+    onEditTodoClick,
+    onDeleteTodoClick,
+    onToggleDoneTodoClick,
+    updateTodoType,
+    clearTodos,
+  } = useTodo();
   return (
     <Container>
       <TodoAddBar onClick={onAddTodoClick} />
       <FilterButtons>
-        <PrimaryButton onClick={() => setTodoType('all')}>
+        <PrimaryButton onClick={() => updateTodoType('all')}>
           全部任務
         </PrimaryButton>
-        <SuccessButton onClick={() => setTodoType('done')}>
+        <SuccessButton onClick={() => updateTodoType('done')}>
           已完成
         </SuccessButton>
-        <InfoButton onClick={() => setTodoType('undone')}>未完成</InfoButton>
-        <DangerButton
-          onClick={() => {
-            setTodoType('all');
-            setTodoList([]);
-          }}
-        >
-          刪除全部
-        </DangerButton>
+        <InfoButton onClick={() => updateTodoType('undone')}>未完成</InfoButton>
+        <DangerButton onClick={clearTodos}>刪除全部</DangerButton>
       </FilterButtons>
       <List>
         {filterTodoList.map((element) => (
